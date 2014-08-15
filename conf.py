f1cd966189bc88c9a11b7baae5eb57c58b2e2a60
Bastian Ballmann <bastian.ballmann@inf.ethz.ch>
2014-08-15 13:09:03 +0200

    updated copyright note

--- conf.py
+++ conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'BaLu Wiki'
-copyright = u'2013, Balle & Lilu'
+copyright = u'2014, Balle & Lilu aka team BaLu'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
